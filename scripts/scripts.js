cd15eeada76e0c768a7d3a7eb4e38746b9394d5b
Prithwee Das <prithweedas123@gmail.com>
2017-11-26 15:42:10 +0530

    smooth scrolling effects & media query for logo

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,12 +3,22 @@ $(window).scroll((e) => {
 
     let landingSectionHeight = $('.landing').height();
     let wScroll = $(e.target).scrollTop();
-
     if (wScroll <= landingSectionHeight) {
 
         $('.logo').css({
             'transform': 'translate(-50%, ' + wScroll / 3 + '%)'
         });
 
+        $('.go-down').css({
+            'transform': 'translate(-50%, ' + wScroll * 3 + '%)'
+        });
+
     }
+});
+
+
+$('.go-down').click((e) => {
+    $('html, body').animate({
+        scrollTop: $('#events-section').offset().top
+    });
 });
