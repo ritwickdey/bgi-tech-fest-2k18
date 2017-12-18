d785ad15f0e0ccb061e761d460b53d83d4dfba27
Ritwick Dey <ritwickdey@outlook.com>
2017-12-18 13:43:09 +0530

    code separated

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -78,4 +78,67 @@ $('document').ready(() => {
     $('.event-details').click((e) => e.stopPropagation());
     $(document).keyup(e => e.keyCode == 27 && openedPopUpEventId && closeModalAction());
 
+    //form 
+    const showConfirmMsg = IsSent => {
+        const successHtml =
+            `
+                <div class="msg-sent-confirm" style="color:green;">
+                    <i class="fa fa-check" aria-hidden="true"></i>
+                    Message Sent!
+                </div>
+            `;
+
+        const failedHtml =
+            `
+                <div class="msg-sent-confirm" style="color:red;">
+                    <i class="fa fa-ban" aria-hidden="true"></i>
+                    Opps! Somthing Went Wrong!
+                </div>
+            `;
+        const targetHtml = IsSent ? successHtml : failedHtml;
+
+        $('#msgSentConfirm').html('');
+        $('#msgSentConfirm').html(targetHtml);
+        $('.msg-sent-confirm').fadeIn();
+        $('.msg-sent-confirm').delay(2000).fadeOut();
+
+    }
+
+    let feedbackForm = document.querySelector('.feedback');
+    feedbackForm.addEventListener('submit', e => {
+        e.preventDefault();
+        let name = document.querySelector('#name');
+        let email = document.querySelector('#email');
+        let phone = document.querySelector('#phone');
+        let message = document.querySelector('#message');
+        let submitBtn = document.getElementById('formSubmit');
+        submitBtn.disabled = true
+        submitBtn.innerHTML = "Sending...";
+        $.ajax({
+            url: "https://formspree.io/prithweedas123@gmail.com",
+            method: "POST",
+            data: {
+                name: name.value,
+                email: email.value,
+                phone: phone.value,
+                message: message.value
+            },
+            dataType: "json",
+            success: () => {
+                showConfirmMsg(true);
+                name.value = "";
+                email.value = "";
+                phone.value = "";
+                message.value = "";
+                submitBtn.disabled = false;
+                submitBtn.innerHTML = "Send Message";
+            },
+            error: () => {
+                showConfirmMsg(false);
+                submitBtn.disabled = false;
+                submitBtn.innerHTML = "Send Message";
+            }
+        });
+    });
+
 });
